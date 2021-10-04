fe52c5321162d12541935a8628e9d49ce728031b
gbrough <gbrough@gmail.com>
2021-10-04 16:07:49 -0700

    create product page and add images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,26 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
-
 function App() {
   return (
     <BrowserRouter>
       <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">Art Store</a>
-        </div>
-        <div>
-        <a href="/cart">Cart</a>   
-            <a href="/signin">Login</a>         
-        </div>
-      </header>
-      <main>
-        <Route path="/product/:id" component= {ProductScreen} ></Route>
-        <Route path="/" component={HomeScreen} exact> </Route>
-
-      </main>
-      <footer className="row center"> All rights reserved. </footer>
-    </div>
-  </BrowserRouter>
+        <header className="row">
+          <div>
+            <a className="brand" href="/"> Art Store</a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
