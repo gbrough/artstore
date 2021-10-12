2128213ac295872bc1d1ed72155531d44be8e844
gbrough <gbrough@gmail.com>
2021-10-12 11:20:30 -0700

    update json package

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ import data from './data.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/artstore' , {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/artstore', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true, 
+  //useCreateIndex: true, //gave an backend terminal error
 });
 
 app.get('/api/products/:id', (req, res) => {
@@ -15,24 +15,23 @@ app.get('/api/products/:id', (req, res) => {
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: 'Product not found' });  
+    res.status(404).send({ message: 'Product Not Found' });
   }
 });
-app.use('/api/users', userRouter);
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
-  });
-
+});
+app.use('/api/users', userRouter);
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-  });
+  res.send('Server is ready');
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;  
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
-  });
+});
