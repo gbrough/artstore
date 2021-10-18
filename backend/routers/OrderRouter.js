a460234ed99cd17b2525fcb8a102064910a46172
gbrough <gbrough@gmail.com>
2021-10-18 10:31:07 -0700

    fix bug with order router

--- backend/routers/OrderRouter.js
+++ backend/routers/OrderRouter.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import expressAsyncHandler from 'express-async-handler';
-import Order from '../models/orderModel.js';
-import { isAuth } from '../utils.js';
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
+import { isAuth } from "../utils.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post(
-'/',
-isAuth,
-expressAsyncHandler(async (req, res) => {
-  if (req.body.orderItems.length === 0) {
-    res.status(400).send({ message: 'Cart is empty' });
+  "/",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    if (req.body.orderItems.length === 0) {
+      res.status(400).send({ message: "Cart is empty" });
     } else {
       const order = new Order({
         orderItems: req.body.orderItems,
@@ -21,13 +21,25 @@ expressAsyncHandler(async (req, res) => {
         taxPrice: req.body.taxPrice,
         totalPrice: req.body.totalPrice,
         user: req.user._id,
-        });
-        const createdOrder = await order.save();
-        res
-          .status(201)
-          .send({ message: 'New Order Created', order: createdOrder });
-      } 
+      });
+      const createdOrder = await order.save();
+      res
+        .status(201)
+        .send({ message: "New Order Created", order: createdOrder });
+    }
   })
-  );
-  
-  export default orderRouter;
+);
+
+orderRouter.get(
+  "/:id",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+      res.status(404).send({ message: "Order not found" });
+    }
+  })
+);
+
+export default orderRouter;
